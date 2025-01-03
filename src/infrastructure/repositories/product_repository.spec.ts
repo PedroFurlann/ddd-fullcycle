89be56214383de8473b393eccf168176a0412d1a
Pedro Furlan <pedrofurlan6@gmail.com>
2025-01-03 00:22:38 -0300

    chore: setup swcrc

--- src/infrastructure/repositories/product_repository.spec.ts
+++ src/infrastructure/repositories/product_repository.spec.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../database/sequelize/model/product.model";
 
 describe("Product Repository Test", () => {
   let sequelize: Sequelize;
@@ -11,8 +12,12 @@ describe("Product Repository Test", () => {
       sync: { force: true },
     });
 
-    afterEach(async () => {
-      await sequelize.close();
-    });
+    sequelize.addModels([ProductModel]);
+
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
   });
 });
